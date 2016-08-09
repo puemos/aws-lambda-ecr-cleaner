7b1b178388d47bfcfaa2dbe67adcd32c779ce514
Shy Alter <puemos@gmail.com>
2016-08-09 20:30:33 +0200

    array of repos

--- Makefile
+++ Makefile
@@ -5,15 +5,15 @@ lambda:
 	@echo "Factory package files..."
 	@if [ ! -d build ] ;then mkdir build; fi
 	@cp index.js build/index.js
-	@cp config.json build/config.json
 	@if [ -d build/node_modules ] ;then rm -rf build/node_modules; fi
 	@cp -R node_modules build/node_modules
-	@cp -R libs build/
-	@cp -R bin build/
-	@rm -rf build/bin/darwin
+	@cp -R lib build/
+	@cp -R config build/
 	@echo "Create package archive..."
 	@cd build && zip -rq aws-lambda-ecr-cleaner.zip .
 	@mv build/aws-lambda-ecr-cleaner.zip ./
+	@echo "clean up package files"
+	@rm -rf build/*
 
 
 
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
-
+const AWS = require('aws-sdk');
+const Promise = require('bluebird');
 const lib = require('./lib');
 const config = require('./config');
 
@@ -9,15 +10,30 @@ module.exports.handler = function (event, context) {
     // Check event for dry run, which prevents actual deletion
     // check logs to see what would have been deleted
     console.log(event);
+    const ecr = Promise.promisifyAll(new AWS.ECR({ region: config.REGION }));
+    const ecs = Promise.promisifyAll(new AWS.ECS({ region: config.ECS_REGION }));
 
-    var params = {
-        repositoryName: config.REPO_TO_CLEAN
-    };
+    if (Array.isArray(config.REPO_TO_CLEAN)) {
+        const tasks = config.REPO_TO_CLEAN
+            .map(repoName => {
+                const config_repo = Object.assign({}, config, {
+                    REPO_TO_CLEAN: repoName
+                });
+                return run(config_repo, ecr, ecs)
+            })
+        return Promise.all(tasks)
+            .then(context.succeed)
+            .catch(context.fail);
+    }
 
-    lib.getRepoImages(params)
-        .then(lib.filterImagesByDateThreshold)
-        .then(lib.filterOutActiveImages)
-        .then(lib.deleteImages)
+    return run(config, ecr, ecs)
         .then(context.succeed)
         .catch(context.fail);
 };
+const run = (config, ecr, ecs) => {
+    return lib.getRepoImages(config, ecr, ecs)
+        // .then(lib.filterImagesByDateThreshold)
+        .then(lib.filterImagesByFirstN(config, ecr, ecs))
+        .then(lib.filterOutActiveImages(config, ecr, ecs))
+        .then(lib.deleteImages(config, ecr, ecs))
+}
