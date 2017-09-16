8c974ef6fa8c7b8bba862265a5d3edff8f4582e5
Shy Alter <puemos@gmail.com>
2017-09-16 21:07:14 +0200

    change AWS_ACCOUNT_ID pri from arn

--- src/configBuilder.js
+++ src/configBuilder.js
@@ -10,9 +10,14 @@ const debug = require('debug')('aws-lambda-ecr-cleaner:configBuilder')
 const configBuilder = (configJson, context) => {
   const AWS_ACCOUNT_ID = context.invoked_function_arn.split(':')[4]
 
-  const config = Object.assign({}, configJson, process.env, {
-    AWS_ACCOUNT_ID
-  })
+  const config = Object.assign(
+    {},
+    {
+      AWS_ACCOUNT_ID
+    },
+    configJson,
+    process.env
+  )
   if (!configJson.REPO_TO_CLEAN) {
     debug('ERROR: NO REPO_TO_CLEAN, must be set')
     throw new Error('Must set REPO_TO_CLEAN')
